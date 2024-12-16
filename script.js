58a2a47c4101c3110b49c667a00ec6c0e30472df
Akfslgn <akifsalgn@gmail.com>
2024-12-16 18:51:08 -0500

    new Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+// Random Card Generator Arrays 
 const suits = ["♥", "♦", "♠", "♣"];
 const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
+// Select elements
 const numberTopElement = document.querySelector(".numbers-top");
 const suitsElement = document.querySelector(".suits");
 const numberBottomElement = document.querySelector(".numbers-bottom");
 
+// Generate random values
 const randomSuit = suits[Math.floor(Math.random() * suits.length)];
 const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
+// Display random values
 numberTopElement.innerText = randomNumber;
 suitsElement.innerText = randomSuit;
 numberBottomElement.innerText = randomNumber;
 
-if (randomSuit === "♥" || randomSuit === "♦") {
-    numberTopElement.style.color = "red";
-    numberBottomElement.style.color = "red";
-    suitsElement.style.color = "red";
-} else {
-    numberTopElement.style.color = "black";
-    numberBottomElement.style.color = "black";
-    suitsElement.style.color = "black";
-}
+// Colorization
+const randomColor = document.querySelector(".card");
+randomColor.style.color = randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
+
+// Alternative Way - Colorization
+
+// if (randomSuit === "♥" || randomSuit === "♦") {
+//     numberTopElement.style.color = "red";
+//     numberBottomElement.style.color = "red";
+//     suitsElement.style.color = "red";
+// } else {
+//     numberTopElement.style.color = "black";
+//     numberBottomElement.style.color = "black";
+//     suitsElement.style.color = "black";
+// }
+
+
+
+
+
+
+
+
+
 
